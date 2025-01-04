a281b2c438642914bea24ac45e09b5bf0bee4b18
Mari <nyalentine@gmail.com>
2025-01-04 18:42:41 +0800

    Count and style changed

--- src/app/components/textarea.tsx
+++ src/app/components/textarea.tsx
@@ -1,18 +1,23 @@
 import { Fira_Code } from "next/font/google";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
-export default function TextareaStyleDefault() {
+export default function TextareaStyleDefault({
+  handleCharCount,
+}: {
+  handleCharCount: (count: number) => void;
+}) {
   return (
-    <textarea
-      rows={10}
-      placeholder="type something..."
-      className={`px-4 py-2 w-full border-2 border-black shadow-md transition focus:outline-none focus:shadow-xs text-2xl ${firaCode.className}`}
-      style={{
-        minWidth: "200px",
-        minHeight: "200px",
-        width: "50%",
-        height: "auto",
-      }}
-    />
+    <div className="min-w-[200px] min-h-[200px] w-1/2 h-auto relative">
+      <div className="w-full bg-black inset-2 absolute h-full" />
+      <textarea
+        onChange={(e) => {
+          const length = e.target.value.length;
+          handleCharCount(length);
+        }}
+        rows={10}
+        placeholder="type something..."
+        className={`p-2 w-full border-2 block border-black relative shadow-md transition focus:outline-none focus:shadow-xs text-2xl ${firaCode.className}`}
+      />
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
+"use client";
+
+import { useState } from "react";
 import SocialMediaCards from "./components/social-media-cards";
 import TextareaStyleDefault from "./components/textarea";
 import { Fira_Code } from "next/font/google";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
 export default function Home() {
+  const [charCount, setCharCount] = useState(0);
+
+  const handleCharCount = (count: number) => {
+    setCharCount(count);
+  };
+
   return (
     <div>
       <h1 className={`text-center text-5xl mt-20${firaCode.className}`}>
         charCount
       </h1>
       <div className="flex justify-center mt-10">
-        <TextareaStyleDefault />
+        <TextareaStyleDefault handleCharCount={handleCharCount} />
       </div>
       <div className="flex justify-center">
-        <SocialMediaCards />
+        <SocialMediaCards charCount={charCount} />
       </div>
     </div>
   );
