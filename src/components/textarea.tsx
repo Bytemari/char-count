e05cf204c36a92b8370f7d840a0ca9793b88383d
Mari <nyalentine@gmail.com>
2025-01-08 03:07:53 +0800

    Added localStorage for save the text in the browser's local storage

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Fira_Code } from "next/font/google";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
@@ -7,12 +8,30 @@ export default function TextareaStyleDefault({
 }: {
   handleCharCount: (count: number) => void;
 }) {
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    const savedText = localStorage.getItem("textareaText");
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (text) {
+      localStorage.setItem("textareaText", text);
+    }
+  }, [text]);
+
   return (
     <div className="min-w-[200px] min-h-[200px] w-1/2 h-auto relative">
       <div className="w-full bg-black inset-2 absolute h-full" />
       <textarea
+        value={text}
         onChange={(e) => {
-          const length = e.target.value.length;
+          const newText = e.target.value;
+          setText(newText);
+          const length = newText.length;
           handleCharCount(length);
         }}
         rows={10}
