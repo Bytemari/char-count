30c2ae4145d8353439a1a107720beeccd45ef000
Mari <nyalentine@gmail.com>
2025-01-30 00:52:08 +0800

    title changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Char Count App",
+  title: "Simple Text Counter!",
   description: "Simple word analytics!",
 };
 
